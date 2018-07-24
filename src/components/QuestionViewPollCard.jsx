828cd8c469b0f7f722fae277652e8f71748c862a
wcwcaseman <40278321+wcwcaseman@users.noreply.github.com>
2018-07-24 17:01:54 -0400

    first data in list
    first data in list

--- src/components/QuestionViewPollCard.jsx
+++ src/components/QuestionViewPollCard.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { Panel, Button,Radio, FormGroup, Grid, Row, Col,DropdownButton,MenuItem } from 'react-bootstrap'
 import ResultBar from './ResultBar'
+import { Link, withRouter } from 'react-router-dom'
 
 class QuestionViewPollCard extends Component {
     render() {
+      const { question, authorsName } = this.props
+
       return(  
       
     <Panel>
-      <Panel.Heading>Person asks:</Panel.Heading>
+      <Panel.Heading>{authorsName}  asks:</Panel.Heading>
       <Panel.Body>
           <Grid>
           <Col sm={2}>
@@ -25,4 +29,51 @@ class QuestionViewPollCard extends Component {
     }
   }
 
-  export default QuestionViewPollCard;
+
+ /*  sarahedo: {
+    id: 'sarahedo',
+    name: 'Sarah Edo',
+    avatarURL: '',
+    answers: {
+      "8xf0y6ziyjabvozdd253nd": 'optionOne',
+      "6ni6ok3ym7mf1p33lnez": 'optionOne',
+      "am8ehyc8byjqgar0jgpub9": 'optionTwo',
+      "loxhs1bqm25b708cmbf3g": 'optionTwo'
+    },
+    questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
+  },
+
+
+  "8xf0y6ziyjabvozdd253nd": {
+    id: '8xf0y6ziyjabvozdd253nd',
+    author: 'sarahedo',
+    timestamp: 1467166872634,
+    optionOne: {
+      votes: ['sarahedo'],
+      text: 'have horrible short term memory',
+    },
+    optionTwo: {
+      votes: [],
+      text: 'have horrible long term memory'
+    }
+  },
+ */
+
+
+//pass in require state objects, then the id prop from the outer container
+function mapStateToProps ({authedUser, users, questions}, { id }) {
+
+  //tweet could be null if we go to a url with an id that doesn't exist
+const question = questions[id]
+const authorsName = users[question.author].name
+//pass the twwet the authed user so we know who is liking the tweet and the tweet info for display
+//formatTweet pass in the tweet, user who made the tweet, current user and parent tweet
+return {
+  authedUser,
+  question: question,
+  authorsName : authorsName
+}
+}
+
+
+  export default withRouter(connect(mapStateToProps)(QuestionViewPollCard)) 
--- src/components/QuestionsList.jsx
+++ src/components/QuestionsList.jsx
@@ -8,43 +8,71 @@ import QuestionViewPollCard from './QuestionViewPollCard'
 class QuestionsList extends Component{
 
     render(){
-        
-        
+               
     return (
     <div>
-            <Panel>
-<Tab.Container id="left-tabs-example" defaultActiveKey="UnansweredQuestions">
-  <Row className="clearfix">
-    <Col sm={12}>
-    <Navbar>
-      <Nav bsStyle="pills">
-        <NavItem eventKey="UnansweredQuestions">Unanswered Questions</NavItem>
-        <NavItem eventKey="AnsweredQuestions">Answered Questions</NavItem>
-      </Nav>
-      </Navbar>
-    </Col>
-    <Col sm={12}>
+      <Panel>
+      <Tab.Container id="left-tabs-example" defaultActiveKey="UnansweredQuestions">
+        <Row className="clearfix">
+          <Col sm={12}>
+          <Navbar>
+            <Nav bsStyle="pills">
+              <NavItem eventKey="UnansweredQuestions">Unanswered Questions</NavItem>
+              <NavItem eventKey="AnsweredQuestions">Answered Questions</NavItem>
+            </Nav>
+            </Navbar>
+          </Col>
+          <Col sm={12}>
 
-      <Tab.Content animation>
-        <Tab.Pane eventKey="UnansweredQuestions">
-        
-            UnansweredQuestions
-            <QuestionViewPollCard/>
-        </Tab.Pane>
-        <Tab.Pane eventKey="AnsweredQuestions">
-        
-            AnsweredQuestions
-            <QuestionViewPollCard/>
-        </Tab.Pane>
-      </Tab.Content>
+            <Tab.Content animation>            
+              <Tab.Pane eventKey="UnansweredQuestions">
+              <ul>
+                  UnansweredQuestions
+                {this.props.questionIds.map((id) => (
+                <li key={id}>
+                    <QuestionViewPollCard id={id}/>
+                 </li>
+                ))}
+              </ul>
 
-    </Col>
-  </Row>
-</Tab.Container>
-</Panel>
+
+              </Tab.Pane>
+              <Tab.Pane eventKey="AnsweredQuestions">
+              <ul>
+                  AnsweredQuestions
+                {this.props.questionIds.map((id) => (
+                  <li key={id}>
+                      <QuestionViewPollCard id={id}/>
+                  </li>
+                ))}
+              </ul>
+
+              </Tab.Pane>
+            </Tab.Content>
+
+          </Col>
+        </Row>
+      </Tab.Container>
+      </Panel>
     </div>
     )
   }
 }
 
-export default connect()(QuestionsList)
+//takes in questions state and returns question ids sorted by timestamp?
+function mapStateToProps ({ questions }) {
+  return {
+    questionIds: Object.keys(questions)
+      .sort((a,b) => questions[b].timestamp - questions[a].timestamp),
+      AnsweredQuestionIds: Object.keys(questions)
+      .sort((a,b) => questions[b].timestamp - questions[a].timestamp),
+      UnansweredQuestionIds: Object.keys(questions)
+      .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+  }
+}
+
+
+
+
+
+export default connect(mapStateToProps)(QuestionsList)
